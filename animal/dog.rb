8b78aabba025daf86ea92da19bb622e8320163e1
Joy Phoebe <joyapisi@gmail.com>
2023-06-20 12:11:01 +0300

    fix linter errors

--- animal/dog.rb
+++ animal/dog.rb
@@ -1,16 +1,16 @@
-require "./animal.rb"
+require './animal'
 
 class Dog < Animal
-  def initialize(color, name = "Unknown")
-    super("dog", 4, name)
+  def initialize(color, name = 'Unknown')
+    super('dog', 4, name)
     @color = color
   end
 
   def bring_a_stick
-    "Here is your stick: ---------"
+    'Here is your stick: ---------'
   end
 
   def speak
-    "Woof, woof"
+    'Woof, woof'
   end
 end
