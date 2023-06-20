4aff34a4e13255ce37137ea50b5b9a401e0b4f44
Joy Phoebe <joyapisi@gmail.com>
2023-06-20 05:34:32 +0300

    add class Person to person.rb

---
+++ person.rb
@@ -0,0 +1,29 @@
+class Person
+    def initialize (name = "Unknown", age, parent_permission = true)
+        @name
+        @age
+        @parent_permission
+    end
+
+    attr_writer :id
+    attr_accessor :name
+    attr_accessor :age
+
+    def of_age?
+        if @age>=18
+            true
+        else 
+            false
+        end
+    end
+
+    def can_use_services?
+        if @parent_permission = true && @age>=18
+            true
+        else
+            false
+        end
+    end
+
+    private :can_use_services?
+end
