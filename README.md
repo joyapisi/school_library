c187af59d2ac7d5a9b763045ef6e25ff49adbd71
Joy Phoebe <joyapisi@gmail.com>
2023-06-20 13:49:03 +0300

    Update readme licence link

--- README.md
+++ README.md
@@ -194,7 +194,7 @@ I'm thankful to Microverse for providing a study platform which guided me throug
 
 ## 📝 License <a name="license"></a>
 
-This project is [MIT](./LICENSE.txt) licensed.
+This project is [MIT](./LICENSE) licensed.
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
--- person.rb
+++ person.rb
@@ -1,20 +1,20 @@
 class Person
+  attr_writer :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  attr_writer :id
-  attr_accessor :name, :age
-
   def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    @parent_permission == true && @age >= 18
+    @parent_permission == true || @age >= 18
   end
 
-  private :can_use_services?
+  private :of_age?
 end
