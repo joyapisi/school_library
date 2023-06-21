8429392b2e773fffa8dc4ba1ea40eedc46cff3db
Joy Phoebe <joyapisi@gmail.com>
2023-06-21 11:37:00 +0300

    🧢 Add operstor to capitalize first name instead of all names

--- capitalize_deco.rb
+++ capitalize_deco.rb
@@ -2,7 +2,7 @@ require './base_deco'
 
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    capitalized = @nameable.correct_name.upcase
+    capitalized = @nameable.correct_name.capitalize
     p capitalized
   end
 end
