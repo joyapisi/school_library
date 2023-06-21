49b58ee218518e6e29ffdd2dabc3c9df9dbb5d4d
Joy Phoebe <joyapisi@gmail.com>
2023-06-21 05:31:04 +0300

    ➕Prepare CapitalizeDecorator and TrimmerDecorator

---
+++ capitalize_deco.rb
@@ -0,0 +1,6 @@
+class CapitalizeDecorator < BaseDecorator
+    def correct_name 
+        capitalized = @nameable.correct_name.upcase
+        p capitalized
+    end
+end
---
+++ trimmer_deco.rb
@@ -0,0 +1,9 @@
+class TrimmerDecorator < BaseDecorator
+    def correct_name 
+       if @nameable.correct_name.length <=10
+        p @nameable.correct_name
+       else
+         trimmed_name = @nameable.correct_name[0, 10].strip
+         p trimmed_name
+       end
+end
