f221242860a8ae557ea52ce61fa1afc6e8a1d632
Joy Phoebe <joyapisi@gmail.com>
2023-06-23 00:42:56 +0300

    add app.rb functions to add person and list all books & people

--- app.rb
+++ app.rb
@@ -1,25 +1,23 @@
 class App 
+    def initialize
+        @books = []
+        @peoples = []
+    end
+
     def list_books()
-        @ = 
+        @books.each { |book| puts "Title: #{book.title} Author: #{book.author} \n" } 
     end
 
     def list_people()
-        
-    end
-
-    def create_person()
-        
-    end
-
-        def create_book()
-        
-    end
-
-        def create_rental()
-        
-    end
-
-        def list_rentals()
-        
+        @peoples.each do |people|
+            puts "[#{people.class.name}] Name: #{people.name} ID: #{people.id} Age: #{people.age} \n"  
     end
 end
+    
+    def create_teacher(age, specialization, parent_permission, name)
+        @peoples << Teacher.new(age, specialization, parent_permission: parent_permission, name: name)
+      end
+
+      def create_student(age, name, parent_permission)
+        @peoples << Student.new(age, name, parent_permission: parent_permission)
+      end
--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
-def main
-    p "Please choose an option by entering a number:"
+def user_options
+    p "Please choose an option by entering a number:
     1 - List all list_books
     2 - List all list_people
-    3 - Create a create_person
-    4 - Create a list_books
-    5 - 
-    end
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - List all rentals for a given person id
+    7 - Exit"
+    number = gets.chomp
+    p "#{number}"
+end
