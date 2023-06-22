308611b4c166874a4c96d98b9374229722c00f02
Joy Phoebe <joyapisi@gmail.com>
2023-06-23 00:32:41 +0300

    Add list of options to user in main.rb

---
+++ app.rb
@@ -0,0 +1,25 @@
+class App 
+    def list_books()
+        @ = 
+    end
+
+    def list_people()
+        
+    end
+
+    def create_person()
+        
+    end
+
+        def create_book()
+        
+    end
+
+        def create_rental()
+        
+    end
+
+        def list_rentals()
+        
+    end
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+def main
+    p "Please choose an option by entering a number:"
+    1 - List all list_books
+    2 - List all list_people
+    3 - Create a create_person
+    4 - Create a list_books
+    5 - 
+    end
