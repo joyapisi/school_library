d19bb13140257eddb2fb690b75a1a4fb2f05cd29
Joy Phoebe <joyapisi@gmail.com>
2023-06-22 18:18:27 +0300

    add rentals to person.rb to achieve has many relationship

--- person.rb
+++ person.rb
@@ -2,12 +2,13 @@ require './nameable'
 
 class Person < Nameable
   attr_writer :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
@@ -24,4 +25,8 @@ class Person < Nameable
   def correct_name
     @name
   end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+ end
 end
