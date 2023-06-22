aee8a05c1b3262124ed17f67093b4f9aff9a5424
Joy Phoebe <joyapisi@gmail.com>
2023-06-23 00:57:01 +0300

    create book or rental

--- main.rb
+++ main.rb
@@ -24,3 +24,35 @@ def create_person(app)
     puts 'Has parent permission?[Y/N]'
     val = gets.chomp
     parent_permission = false if %w[N n NO no No].include?(val)
+    case option.to_i
+    when 1
+      app.create_student(age, name, parent_permission)
+    when 2
+      puts 'specialization:-'
+      specialization = gets.chomp
+      app.create_teacher(age, specialization, name, parent_permission)
+    else
+      puts 'Invalid input ):'
+    end
+  end
+
+  def handel_option(option, app)
+    case option
+    when 3
+      create_person(app)
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      puts 'Enter person ID:-'
+      id = gets.chomp.to_i
+      app.list_person_rentals(id)
+    when 7
+      puts 'Thanks You!!...'
+      return false
+    else
+      puts 'Invalid option. Please try again.'
+    end
+    true
+  end
